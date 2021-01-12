eb5bd15e8ef4d01cdc98477ef58ddee47050bbb9
f0lg0 <folgonileonardo@gmail.com>
2021-01-12 17:59:18 +0100

    new: initial setup

---
+++ web/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <Navbar />
+        <router-view />
+    </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+    name: "Home",
+    components: {
+        Navbar
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+</style>
