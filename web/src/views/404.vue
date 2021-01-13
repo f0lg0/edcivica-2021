d45fcf15ca7c3440a35397908a553aa370dcae95
f0lg0 <folgonileonardo@gmail.com>
2021-01-13 21:35:18 +0100

    new: 404 page

---
+++ web/src/views/404.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="not-found">
+        <p><span class="hl">[edcivica]$</span> curl {{ this.$route.path }}</p>
+        <p><span class="err">404:</span> Pagina non trovata</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#not-found {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+#not-found p {
+    font-size: 40px;
+    text-align: center;
+}
+
+.hl {
+    color: var(--accent-main-green);
+    font-weight: bold;
+}
+
+.err {
+    color: var(--accent-main-red);
+    font-weight: bold;
+}
+</style>
