2023d75772de8428d0e71ac4775da6fa635bfed5
f0lg0 <folgonileonardo@gmail.com>
2021-01-13 22:08:58 +0100

    new: home page banner

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <a href="https://www.freepik.com/vectors/technology"
+            >Technology vector created by stories - www.freepik.com</a
+        >
     </div>
 </template>
 
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="home">
-        <h1 id="title">
-            <TextGlitch
-                text="Introduzione"
-                background="var(--main-black)"
-                fill="var(--accent-main-green)"
-                highlight1="red"
-                highlight2="blue"
-            ></TextGlitch>
-        </h1>
+        <div class="banner">
+            <div class="intro">
+                <h1 id="title">
+                    <TextGlitch
+                        text="Educazione Civica."
+                        background="var(--main-black)"
+                        fill="var(--accent-main-green)"
+                        highlight1="red"
+                        highlight2="blue"
+                    ></TextGlitch>
+                </h1>
+                <p id="subtitle">
+                    Scegli quali tracce lasciare di te stesso <br />
+                    nei computer degli altri.
+                </p>
+            </div>
+
+            <div class="pic">
+                <img src="../assets/banner.png" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -23,8 +35,40 @@ export default {
 </script>
 
 <style scoped>
+.banner {
+    width: 100%;
+    height: 300px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 70px;
+}
+.pic {
+    width: 600px;
+    height: 600px;
+    margin-left: 150px;
+}
+
+.pic img {
+    width: 100%;
+    height: 100%;
+}
+
+.intro {
+    width: 40%;
+}
 #title {
-    font-size: 100px;
-    text-align: center;
+    font-size: 70px;
+    text-align: left;
+    font-weight: 900;
+    margin-bottom: 0;
+}
+
+#subtitle {
+    font-size: 30px;
+    text-align: left;
+    font-weight: 300;
+    color: var(--main-font-color);
+    margin-top: 20px;
 }
 </style>
